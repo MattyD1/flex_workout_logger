6db48687c34275fc29c59154bdfcabaea33ec32e
Matt Dunn <mattdunn0816@gmail.com>
2024-01-06 10:44:41 -0700

    Added domain layer

---
+++ lib/features/exercises/domain/entities/exercise_entity.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'exercise_entity.freezed.dart';
+
+/// Strongly Typed Model [ExerciseEntity]
+@freezed
+class ExerciseEntity with _$ExerciseEntity {
+  /// [ExerciseEntity] factory constructor
+  const factory ExerciseEntity({
+    required String id,
+    required String name,
+  }) = _ExerciseEntity;
+}
---
+++ lib/features/exercises/domain/validations/exercise_name.dart
@@ -0,0 +1,37 @@
+import 'package:flex_workout_logger/features/common/domain/errors/failure.dart';
+import 'package:flex_workout_logger/features/common/domain/validations/validation_abstract.dart';
+import 'package:fpdart/fpdart.dart';
+
+/// Department Name value
+class DepartmentName extends Validation<String> {
+  ///
+  factory DepartmentName(String input) {
+    return DepartmentName._(
+      _validate(input),
+    );
+  }
+
+  const DepartmentName._(this._value);
+  @override
+  Either<Failure, String> get value => _value;
+
+  final Either<Failure, String> _value;
+}
+
+Either<Failure, String> _validate(String input) {
+  if (input.length < 2) {
+    return left(
+      const Failure.unprocessableEntity(
+        message: 'The name must be at least 2 characters in length',
+      ),
+    );
+  } else if (input.length > 100) {
+    return left(
+      const Failure.unprocessableEntity(
+        message: 'The name must be less than 100 characters in length',
+      ),
+    );
+  }
+
+  return right(input);
+}
