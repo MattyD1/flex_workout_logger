0a3f6b5eb60d663de7b20a0a3cc3aecd414234e9
Matt Dunn <mattdunn0816@gmail.com>
2024-01-07 13:43:43 -0700

    Added exercise list

--- lib/features/exercises/ui/widgets/exercises_list.dart
+++ lib/features/exercises/ui/widgets/exercises_list.dart
@@ -1,5 +1,7 @@
+import 'package:flex_workout_logger/features/common/ui/utils/ui_extensions.dart';
 import 'package:flex_workout_logger/features/common/ui/widgets/app_error.dart';
 import 'package:flex_workout_logger/features/exercises/controllers/exercises_list_controller.dart';
+import 'package:flex_workout_logger/features/exercises/ui/widgets/exercise_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,9 +24,17 @@ class ExercisesList extends ConsumerWidget {
           : Column(
               children: [
                 ...items.map(
-                  (e) => ListTile(
-                    title: Text(e.name),
-                    onTap: () {/* Handle item tap */},
+                  (e) => Column(
+                    children: [
+                      ExercisesCard(
+                        exercise: e,
+                      ),
+                      Divider(
+                        color: context.colorScheme.divider,
+                        height: 1,
+                        indent: 64,
+                      ),
+                    ],
                   ),
                 ),
               ],
