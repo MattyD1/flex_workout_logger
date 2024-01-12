11278632c4757060589debcdda5a13e3ee9c26b2
Matt Dunn <mattdunn0816@gmail.com>
2024-01-12 16:01:58 -0700

    Finished Movement Pattern domain

---
+++ lib/features/exercises/domain/repositories/movement_pattern_interface.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flex_workout_logger/features/exercises/domain/entities/movement_pattern_entity.dart';
+import 'package:flex_workout_logger/features/exercises/domain/validations/movement_pattern_description.dart';
+import 'package:flex_workout_logger/features/exercises/domain/validations/movement_pattern_name.dart';
+import 'package:flex_workout_logger/utils/failure.dart';
+import 'package:fpdart/fpdart.dart';
+
+/// MovementPattern Repository Interface
+abstract class IMovementPatternRepository {
+  /// Get MovementPattern list
+  FutureOr<Either<Failure, List<MovementPatternEntity>>> getMovementPatterns();
+
+  /// Get MovementPattern by id
+  FutureOr<Either<Failure, MovementPatternEntity>> getMovementPatternById(
+    String id,
+  );
+
+  /// Create MovementPattern
+  FutureOr<Either<Failure, MovementPatternEntity>> createMovementPattern(
+    MovementPatternName name,
+    MovementPatternDescription description,
+  );
+
+  /// Update MovementPattern
+  FutureOr<Either<Failure, MovementPatternEntity>> updateMovementPattern(
+    String id,
+    MovementPatternName name,
+    MovementPatternDescription description,
+  );
+
+  /// Delete MovementPattern by id
+  FutureOr<Either<Failure, bool>> deleteMovementPattern(String id);
+}
---
+++ lib/features/exercises/domain/validations/movement_pattern_description.dart
@@ -0,0 +1,31 @@
+import 'package:flex_workout_logger/utils/failure.dart';
+import 'package:flex_workout_logger/utils/validation_abstract.dart';
+import 'package:fpdart/fpdart.dart';
+
+/// Department Name value
+class MovementPatternDescription extends Validation<String> {
+  ///
+  factory MovementPatternDescription(String input) {
+    return MovementPatternDescription._(
+      _validate(input),
+    );
+  }
+
+  const MovementPatternDescription._(this._value);
+  @override
+  Either<Failure, String> get value => _value;
+
+  final Either<Failure, String> _value;
+}
+
+Either<Failure, String> _validate(String input) {
+  if (input.length > 250) {
+    return left(
+      const Failure.unprocessableEntity(
+        message: 'The description must be less than 250 characters in length',
+      ),
+    );
+  }
+
+  return right(input);
+}
