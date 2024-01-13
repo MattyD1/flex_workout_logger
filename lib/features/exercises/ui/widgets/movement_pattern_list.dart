d024dfd4d99ae7bb8ea0bcc829d6f630d852188c
Mitchell Joram <mitchell.a.joram@gmail.com>
2024-01-13 16:35:07 -0700

    Finished movement pattern list screen

--- lib/features/exercises/ui/widgets/movement_pattern_list.dart
+++ lib/features/exercises/ui/widgets/movement_pattern_list.dart
@@ -1,5 +1,7 @@
 import 'package:flex_workout_logger/config/theme/app_layout.dart';
 import 'package:flex_workout_logger/features/exercises/controllers/movement_pattern_list_controller.dart';
+import 'package:flex_workout_logger/features/exercises/ui/widgets/movement_pattern_card.dart';
+import 'package:flex_workout_logger/utils/ui_extensions.dart';
 import 'package:flex_workout_logger/widgets/app_error.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -23,7 +25,18 @@ class MovementPatternList extends ConsumerWidget {
             )
           : Column(
               children: [
-                ...items.map((e) => Text(e.name)),
+                ...items.map((mp) => Column(
+                  children: [
+                    MovementPatternCard(
+                      movementPattern: mp
+                    ),
+                    Divider(
+                      color: context.colorScheme.divider,
+                      height: 1,
+                      indent: 64,
+                    ),
+                  ],
+                )),
               ],
             ),
       error: (o, e) => AppError(
