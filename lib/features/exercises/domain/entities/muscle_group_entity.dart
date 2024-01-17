23e7c451f2ad46432a9cb6c783c72cb888d0db7e
Matt Dunn <mattdunn0816@gmail.com>
2024-01-17 09:48:32 -0700

    Added muscle group freezed entity

---
+++ lib/features/exercises/domain/entities/muscle_group_entity.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'muscle_group_entity.freezed.dart';
+
+/// Strongly Typed Model [MuscleGroupEntity]
+@freezed
+class MuscleGroupEntity with _$MuscleGroupEntity {
+  /// [MuscleGroupEntity] factory constructor
+  const factory MuscleGroupEntity({
+    required String id,
+    required String name,
+    required String description,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+    List<String>? exerciseIds,
+  }) = _MuscleGroupEntity;
+}
